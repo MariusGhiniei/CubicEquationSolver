7062c1621ec497b8d611511af0ae46f3decf8378
MariusGhiniei <40267036+MariusGhiniei@users.noreply.github.com>
2022-05-17 12:40:13 +0300

    Add files via upload
    push all

---
+++ CubicEquationSolver/Cubic.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Numerics;
+
+namespace CubicEquationSolver
+{
+    public class Cubic
+    {
+        const double ZeroAlmost = 1.0E-8; // 0.000000001
+        const double Third = 1.0 / 3.0; // 0.333333
+
+        public Complex[] Solve(double a, double b, double c, double d)
+        {
+            //Cardano's Method
+
+            if (a == 0)
+            {
+                //=> bx^2 + cx + d = 0
+                return null;
+            }
+
+            //Cardano constants
+            double j = b / a;
+            double k = c / a;
+            double l = d / a;
+            double p = -(j * j / 3.0) + k;
+            double q = (2.0 / 27.0 * j * j * j) - (j * k / 3.0) + l;
+            double t = q * q / 4.0 + p * p * p / 27.0;
+
+            // force to be absolutely 0 to numbers very close to 0
+            if (Math.Abs(t) < ZeroAlmost)
+            {
+                t = 0;
+            }
+            if (Math.Abs(q) < ZeroAlmost)
+            {
+                q = 0;
+            }
+
+            double r1 = 0, r2 = 0, r3 = 0, i1 = 0, i2 = 0, i3 = 0, i;
+
+            if (t > 0)
+            {
+                //if t positive => 1 solution real and 2 complex
+                // real solution
+
+                r1 = CubeRoot(-q / 2.0 + Math.Sqrt(t))
+                   + CubeRoot(-q / 2.0 - Math.Sqrt(t));
+
+                // two complex solutions
+                r2 = -r1 / 2.0;
+                r3 = r2;
+
+                // imaginary
+                if (q == 0)
+                {
+                    i = Math.Sqrt(k);
+                }
+                else
+                {
+                    i = Math.Sqrt(Math.Abs(Math.Pow(r1 / 2.0, 2.0) + q / r1));
+                }
+
+                i1 = 0;
+                i2 = i;
+                i3 = -i;
+            }
+            if (t == 0)
+            {
+                // 3 real solutions, at least 2 are equal
+                r1 = 2.0 * CubeRoot(-q / 2.0);
+                r2 = -r1 / 2.0 + Math.Sqrt(Math.Pow(r1 / 2.0, 2.0) + q / r1);
+                r3 = -r1 / 2.0 - Math.Sqrt(Math.Pow(r1 / 2.0, 2.0) + q / r1);
+            }
+            if (t < 0)
+            {
+                //for t negative => all solutions are real
+                double x = -q / 2.0;
+                double y = Math.Sqrt(-t); // we can't work with negative values => t positive
+                double angle = Math.Atan(y / x);
+
+                if (q > 0)
+                {
+                    // for q positive => angle = 2*PI - angle 
+                    angle = 2 * Math.PI - angle; // ??????????????????
+                }
+
+                r1 = 2.0 * Math.Sqrt(-p / 3.0) * Math.Cos(angle / 3.0);
+                r2 = 2.0 * Math.Sqrt(-p / 3.0) * Math.Cos((angle + 2.0 * Math.PI) / 3.0);
+                r3 = 2.0 * Math.Sqrt(-p / 3.0) * Math.Cos((angle + 4.0 * Math.PI) / 3.0);
+            }
+            // the solutions: 
+            Complex[] result = new Complex[3];
+
+            result[0] = new Complex(r1 - j / 3.0, i1);
+            result[1] = new Complex(r2 - j / 3.0, i2);
+            result[2] = new Complex(r3 - j / 3.0, i3);
+
+            return result;
+        }
+
+        double CubeRoot(double number)
+        {
+            if (number < 0)
+                return -Math.Pow(-number, Third);
+            else
+                return Math.Pow(number, Third);
+        }
+    }
+}
---
+++ CubicEquationSolver/CubicSolverForm.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Numerics;
+using System.Windows.Forms;
+
+namespace CubicEquationSolver
+{
+    public partial class CubicSolverForm : Form
+    {
+        readonly Cubic _cubic = new Cubic();
+        Complex[] _solutions;
+
+        double A
+        {
+            get => double.Parse(textBox2.Text);
+            set => textBox2.Text = value.ToString();
+        }
+
+        double B
+        {
+            get => double.Parse(textBox3.Text);
+            set => textBox3.Text = value.ToString();
+        }
+
+        double C
+        {
+            get => double.Parse(textBox4.Text);
+            set => textBox4.Text = value.ToString();
+        }
+
+        double D
+        {
+            get => double.Parse(textBox5.Text);
+            set => textBox5.Text = value.ToString();
+        }
+
+        string X1
+        {
+            get => textBox6.Text;
+            set => textBox6.Text = value;
+        }
+        string X2
+        {
+            get => textBox7.Text;
+            set => textBox7.Text = value;
+        }
+        string X3
+        {
+            get => textBox8.Text;
+            set => textBox8.Text = value;
+        }
+
+
+        public CubicSolverForm()
+        {
+            InitializeComponent();
+            button1.Click += (s, e) => Execute();
+        }
+        private void Execute()
+        {
+            _solutions = _cubic.Solve(A, B, C, D);
+            if (_solutions != null)
+            {
+                X1 = ComplexToString(_solutions[0]);
+                X2 = ComplexToString(_solutions[1]);
+                X3 = ComplexToString(_solutions[2]);
+            }
+            else
+            {
+                X1 = "null";
+                X2 = "null";
+                X3 = "null";
+            }
+
+        }
+
+        string ComplexToString(Complex z)
+        {
+            int len = 5;
+            string s = $"N{len}"; // 5 decimals for solutions
+            if (z.Imaginary == 0)
+            {
+                return z.Real.ToString(s);
+            }
+
+            if (z.Real == 0)
+            {
+                return z.Imaginary.ToString(s) + "i";
+            }
+
+            return (z.Real == 0 ? "" : z.Real.ToString(s)) +
+                   (z.Imaginary >= 0 ? " + " : " - ") +
+                   Math.Abs(z.Imaginary).ToString(s) + "i";
+        }
+       
+
+        private void CubicSolverForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
